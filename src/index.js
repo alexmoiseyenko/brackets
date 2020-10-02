9bed594144a9be941a7b8772510d080cf360e313
Alex <beldeut@gmail.com>
2020-10-02 21:54:14 +0300

    feat: add solution for Brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let res = [];
+    if (str.length % 2 !== 0) {
+        return false;
+    }
+    str.split('').forEach((item) => {
+        bracketsConfig.forEach((i) => {
+            if (i[1] === item && item === (res[res.length - 1] || 0)) {
+                res.pop();
+            } else if (i[0] === item) {
+                res.push(i[1]);
+            }
+        })
+    })
+
+    return res.length === 0;
 }
+
